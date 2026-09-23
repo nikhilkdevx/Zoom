18b515b347e20268b5cb600a99abd6d1c641232d
Nikhil Kumar <nkdevx.tech@gmail.com>
2026-09-23 14:07:09 +0530

    connected backedn to mdb

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -3,6 +3,15 @@ const app = express();
 
 const PORT = 9090;
 
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.1:27017/zoom")
+.then(()=>{
+    console.log("MONGODB CONNECTED");
+})
+.catch((err)=>{
+    console.log("MONGODB CONNECTION ERROR: ",err);
+})
+
 app.listen(PORT,()=>{
     console.log(`APP IS LISTENING TO ${PORT}`);
 });
