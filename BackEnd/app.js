9154eaa0e0318903dd1b590ee9860d04ba1c6f88
Nikhil Kumar <nkdevx.tech@gmail.com>
2026-09-24 19:39:04 +0530

    completed the login logic

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -5,11 +5,14 @@ const User = require("./Models/user");
 const bcrypt = require("bcrypt");
 const ExpressError = require("./utilis/ExpressError");
 const { StatusCodes } = require("http-status-codes");
-
+const jwt = require("jsonwebtoken");
 const validateRegisteration = require("./Validators/validateRegisteration");
 const validateLogin = require("./Validators/validateLogin");
 
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
 
 mongoose.connect("mongodb://127.0.0.1:27017/zoom")
 .then(()=>{
@@ -57,9 +60,32 @@ app.post("/auth/register",async (req,res)=>{
     return res.status(StatusCodes.CREATED).json({message : "User Registered",safeUser});
 });
 
-app.post("/auth/login",(req,res)=>{
+app.post("/auth/login",async (req,res)=>{
     const result = validateLogin.validate(req.body);
-})
+    if(result.error){
+        throw new ExpressError(StatusCodes.BAD_REQUEST,result.error.message);
+    }
+    const {email,password} = req.body;
+    const user = await User.findOne({email});
+    if(!user){
+        throw new ExpressError(StatusCodes.UNAUTHORIZED,"Invalid Email or Password");
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch){
+        throw new ExpressError(StatusCodes.UNAUTHORIZED,"Invalid Email or Password");
+    }
+    const token = jwt.sign(
+        { userId : user._id},
+        process.env.JWT_SECERT,
+        {expiresIn : "7d"}
+    );
+    const safeUser = {
+        id : user._id,
+        name : user.name,
+        email: user.email
+    };
+    res.status(StatusCodes.OK).json({message : "Login Success" , token , user : safeUser});
+});
 
 // ERROR HANDLING LOGIC
 app.use((err,req,res,next)=>{
